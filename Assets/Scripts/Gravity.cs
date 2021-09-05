373bbc7a848c344c721bd7f2c1ca07b1af103958
Mohamad Moheb <mohamadmoheb7@gmail.com>
2021-09-05 03:46:55 +0300

    Create Gravity.cs

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+    
+    private float gravity = 9.81F;
+
+    public void Attract(Transform playerTransform)
+    {
+        Vector3 gravityUp = (playerTransform.position - transform.position).normalized;
+        Vector3 localUp = playerTransform.up;
+
+        playerTransform.GetComponent<Rigidbody>().AddForce(gravityUp * gravity);
+
+        Quaternion targetRotation = Quaternion.FromToRotation(localUp, gravityUp) * playerTransform.rotation;
+        playerTransform.rotation = Quaternion.Slerp(playerTransform.rotation, targetRotation, 50f * Time.deltaTime);
+    }
+}
