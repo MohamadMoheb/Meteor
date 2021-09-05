fc711f983763de49042bd5ae9828ad634e570c70
Mohamad Moheb <mohamadmoheb7@gmail.com>
2021-09-05 03:58:43 +0300

    Create Player.cs

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Gravity attractorPlanet;
+    public float moveSpeed;
+
+    private Transform playerTransform;
+    private Vector3 moveDirection;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+
+        playerTransform = transform;
+    }
+
+    void Update()
+    {
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.TransformDirection(moveDirection) * moveSpeed * Time.deltaTime);
+
+        if (attractorPlanet)
+        {
+            attractorPlanet.Attract(playerTransform);
+        }
+    }
+}
